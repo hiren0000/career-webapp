1e6588818bd871180fde580f82d374b9ebd2684e
Hiren Devmurari <rebeldevmurari@gmail.com>
2024-03-15 22:08:40 +0000

    added dynamic data and handle at the front hand side as well :)

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 
 app = Flask(__name__)
+
+Jobs = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 'Rs. 12,00,000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': '$ 1,20,000'
+}]
 
 
 @app.route("/")
 def hello():
-  return "<p>Hello, World!</p>"
+  return render_template("home.html", jobs=Jobs)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(Jobs)
 
 
 if __name__ == "__main__":
